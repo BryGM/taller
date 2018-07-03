81170921b62700fcd8ab2b371343ba54c44659ad
Bryan Emmanuel Garcia <BryGM>
2018-07-03 12:19:31 -0500

    Primera version

---
+++ Practica.java
@@ -0,0 +1,8 @@
+package estructurados;
+
+public class HolaMundo{
+	public static void mian (String [] arg){
+	System.out.println("Hola mundo ");
+	new java.util.Scanner (System.in).nextLine();
+	}
+}
