9cd9f5db7a961b161245dd138dc9cda9784ba0b6
BryGM <40803315+BryGM@users.noreply.github.com>
2018-07-03 12:36:32 -0500

    Update Practica.java

--- Practica.java
+++ Practica.java
@@ -3,6 +3,7 @@ package estructurados;
 public class HolaMundo{
 	public static void mian (String [] arg){
 	System.out.println("Hola mundo ");
+		System.out.println("Hola2 ");
 	new java.util.Scanner (System.in).nextLine();
 	}
 }
